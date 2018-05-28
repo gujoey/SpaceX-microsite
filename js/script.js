b49d03f7398364e0828b2724fabb210e91311051
Øyvind Gujord <oyvind_gujord@hotmail.com>
2018-05-28 19:24:31 +0200

    site build

--- js/script.js
+++ js/script.js
@@ -1,17 +1,37 @@
-//hamburger to X on click
+//hamburger menu to X on click
 document.querySelector("#nav-toggle").addEventListener("click", function(){
 	this.classList.toggle("active");
 });
 
 
-//initializes fullPage.js
+//initialize fullPage.js
 $(document).ready(function() {
-	$('#fullpage').fullpage();
+	$('#fullpage').fullpage({
+		anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'lastPage'],
+		menu: '#myMenu',
+		navigation: true
+	});
 });
 
 
-$('#fullpage').fullpage({
-	anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
-	menu: '#myMenu',
-	navigation: true
-});
+//initialize google maps js API
+/*
+function initMap(){
+	let location = {
+		lat: -28.037,
+		lng: -147.354
+	};
+
+	let map = new google.maps.Map(document.getElementById("map"), {
+		zoom: 3,
+		center: location
+	});
+
+	let marker = new google.maps.Marker({
+		position: location,
+		icon: 'iss.png',
+		map: map,
+		title: 'International Space Station (ISS)'
+	});
+}
+*/
